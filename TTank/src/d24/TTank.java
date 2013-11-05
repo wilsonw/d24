c95c30f5888bac13e9db8a732ce0a55201b1ab79
Trung Nguyen <trung.n.k@gmail.com>
2013-11-05 22:51:11 +0800

    AdvancedRobot avoiding wall contact

--- TTank/src/d24/TTank.java
+++ TTank/src/d24/TTank.java
@@ -3,39 +3,97 @@
  */
 package d24;
 
-import robocode.BulletHitBulletEvent;
-import robocode.BulletHitEvent;
-import robocode.Robot;
+import robocode.AdvancedRobot;
+import robocode.HitWallEvent;
 import robocode.ScannedRobotEvent;
 
 /**
+ * 1. Use advancedrobot for unblocking calls
+ * 2. Avoid wall contact during the move
+ * 
  * @author trung
  *
  */
-public class TTank extends Robot {
+public class TTank extends AdvancedRobot {
+
+	@Override
 	public void run() {
 		while (true) {
-			sirYesSir();
+			// turn perpendicular
+			if (getHeading() <= 90) {
+				turnLeft(getHeading());
+			} else if (getHeading() <= 180) {
+				turnRight(180 - getHeading());
+			} else if (getHeading() <= 270) {
+				turnLeft(getHeading() - 180);
+			} else {
+				turnRight(360 - getHeading());
+			}
+			
+			toNearestWall();
+			
+//			turnGunRight(90);
+			
+			execute();
 		}
 	}
 	
-	private void sirYesSir() {
-		ahead(100);
-		turnRight(30);
+	private void toNearestWall() {
+		double fieldWidth = getBattleFieldWidth();
+		double fieldHeight = getBattleFieldHeight();
+		double currentX = getX();
+		double currentY = getY();
+		double topGap = fieldHeight - currentY;
+		double bottomGap = currentY;
+		double leftGap = currentX;
+		double rightGap = fieldWidth - currentX;
+		
+		if (topGap <= bottomGap && topGap <= Math.min(leftGap, rightGap)) {
+			double a = (int) getHeading() > 0 ? -1 : 1;
+			ahead(adjustAhead(topGap, 100) * a);
+		}
+		
+		if (bottomGap <= topGap && bottomGap <= Math.min(leftGap, rightGap)) {
+			double a = (int) getHeading() > 0 ? 1 : -1;
+			ahead(adjustAhead(bottomGap, 100) * a);
+		}
+		
+		if (leftGap <= rightGap && leftGap <= Math.min(topGap, bottomGap)) {
+			if ((int) getHeading() > 0) {
+				turnRight(90);
+			} else {
+				turnLeft(90);
+			}
+			ahead(adjustAhead(leftGap, 100));
+		}
+		
+		if (rightGap <= leftGap && rightGap <= Math.min(topGap, bottomGap)) {
+			if ((int) getHeading() > 0) {
+				turnLeft(90);
+			} else {
+				turnRight(90);
+			}
+			ahead(adjustAhead(rightGap, 100));
+		}
 	}
 	
+	/**
+	 * This adjusts the travel distance before it hits the wall
+	 * 
+	 * @param maxDistance
+	 * @param i 
+	 */
+	private double adjustAhead(double gap, double maxDistance) {
+		return Math.min(Math.max(gap - 18, 0), maxDistance);
+	}
+
 	@Override
 	public void onScannedRobot(ScannedRobotEvent event) {
-		fire(1);
+//		fire(1);
 	}
 	
 	@Override
-	public void onBulletHit(BulletHitEvent event) {
-		back(100);
-	}
-	
-	@Override
-	public void onBulletHitBullet(BulletHitBulletEvent event) {
-		super.onBulletHitBullet(event);
+	public void onHitWall(HitWallEvent event) {
+		
 	}
 }
